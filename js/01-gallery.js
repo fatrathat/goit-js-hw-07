e3c6547695dc620061894583397bb4007a41edf2
Eugene Multan <eugene.multan@gmail.com>
2022-05-01 23:15:16 +0300

    1-gallery done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
+
+const insertGalleryItem = (items) => {
+  return items
+    .map(
+      (item) => `<div class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">
+                <img 
+                    class="gallery__image" 
+                    src="${item.preview}" 
+                    data-source="${item.original}" 
+                    alt="${item.description}"
+                /> 
+            </a>
+            </div>`
+    )
+    .join("");
+};
+
+const onGalleryItem = (item) => {
+  item.preventDefault();
+  const originalSize = item.target.dataset.source;
+  const instance = basicLightbox.create(
+    `
+    <img src="${originalSize}" width="800" height="600">`
+  );
+
+  instance.show();
+};
+
+refs.gallery.addEventListener("click", onGalleryItem);
+refs.gallery.insertAdjacentHTML("beforeend", insertGalleryItem(galleryItems));
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
+
+const insertGalleryItem = (items) => {
+  return items
+    .map(
+      (item) => `<a class="gallery__item" href="large-image.jpg">
+                        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+                    </a>`
+    )
+    .join("");
+};
+
+const gallery = new SimpleLightbox(".gallery a");
+
+gallery.on("show.simplelightbox", () => {
+  gallery.open();
+});
+
+refs.gallery.insertAdjacentHTML("beforeend", insertGalleryItem(galleryItems));
